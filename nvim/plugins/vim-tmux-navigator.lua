6b915137bbf977555c72f7be8c8dd0ed721c63ee
Yaohua <zhangyaohua40605@gmail.com>
2026-01-01 09:54:41 +0800

    solved bugs about tmux-vim navigation

--- nvim/plugins/vim-tmux-navigator.lua
+++ nvim/plugins/vim-tmux-navigator.lua
@@ -1,21 +1,18 @@
-local disable = true
-
-if disable then
-  return {
-    "numToStr/Navigator.nvim",
-    vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>"),
-    vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>"),
-    vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>"),
-    vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>"),
-    vim.keymap.set({ "n", "t" }, "<C-p>", "<CMD>NavigatorPrevious<CR>"),
-  }
-end
-
 return {
-
   "christoomey/vim-tmux-navigator",
-  vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true }),
-  vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true }),
-  vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true }),
-  vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true }),
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
